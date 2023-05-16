3acdb64291aa13b5fe3abb30d694a0de49e6aec2
Alexander <elmo.loves.xander@gmail.com>
2023-05-16 13:38:43 +0800

    update payload

--- requestgpt.py
+++ requestgpt.py
@@ -84,14 +84,22 @@ def get_gpt_translation(input, length="medium", timeout=7):
 
     payload_long = {
         "messages": [
-            # {
-            #     "role": "user",
-            #     "content": "translate the following to English:\"さっきからこいつの思考が全く読めない\" \n Reply with only the translation, followed by a full in depth explanation starting with \"Full explanation:\"."
-            # },
-            # {
-            #     "role": "assistant",
-            #     "content": "I haven't been able to read this person's thoughts at all since a while ago. \nFull explanation: The speaker is expressing their inability to understand or predict what someone else is thinking."
-            # },
+            {
+                "role": "user",
+                "content": "translate the following to English:\"さっきからこいつの思考が全く読めない\" \n Reply with only the translation, followed by a full in depth explanation starting with \"Full explanation:\"."
+            },
+            {
+                "role": "assistant",
+                "content": """I haven't been able to read this person's thoughts at all since a while ago. \nFull explanation: 
+1. さっきから (sakki kara): "since a while ago" or "for some time now"
+2. こいつ (koitsu): "this guy" or "this person" (informal and somewhat rude)
+3. の (no): possessive particle, similar to "'s" or "of" in English
+4. 思考 (shikō): "thought" or "thinking"
+5. が (ga): subject particle
+6. 全く (mattaku): "completely" or "entirely"
+7. 読めない (yomenai): negative potential form of the verb 読む (yomu, "to read"), meaning "can't read"
+The speaker is expressing their inability to understand or predict what the other person is thinking."""
+            },
             # {
             #     "role": "user",
             #     "content": "translate the following to English:\"球磨川が目の前にいるってのに！どうしてブルってんだよ俺の身体！！\" \n Reply with only the translation, followed by a full in depth explanation starting with \"Full explanation:\"."
@@ -106,11 +114,12 @@ def get_gpt_translation(input, length="medium", timeout=7):
             },
             {
                 "role": "assistant",
-                "content": """This is the price I paid for my own mistakes. \nFull explanation: 1. これは (Kore wa): This means "this is" and is used to refer to something that has been mentioned or is being discussed.
+                "content": """This is the price I paid for my own mistakes. \nFull explanation: 
+1. これは (Kore wa): This means "this is" and is used to refer to something that has been mentioned or is being discussed.
 2. 僕が (boku ga): "僕" (boku) is a first-person pronoun meaning "I" or "me," and "が" (ga) is a subject marker. Together, they mean "I" as the subject of the sentence.
 3. 己の過ちに対して (onore no ayamachi ni taishite): "己" (onore) is a pronoun meaning "oneself" or "myself." "過ち" (ayamachi) means "mistake" or "error." "に対して" (ni taishite) is a preposition meaning "toward" or "in relation to." So, this phrase means "toward my own mistakes."
 4. 支払った代償であり (shiharatta daishou de ari): "支払った" (shiharatta) is the past tense of the verb "支払う" (shiharau), which means "to pay" or "to compensate." "代償" (daishou) means "price" or "compensation." "であり" (de ari) is a formal way to say "is" or "to be." This phrase means "the price I paid."
-Putting it all together, the sentence これは僕が己の過ちに対して支払った代償であり (Kore wa boku ga onore no ayamachi ni taishite shiharatta daishou de ari) means "This is the price I paid for my own mistakes." It conveys a sense of responsibility and acknowledgment of one's own errors and the consequences that followed."""
+The speaker is acknowledging that the situation they are facing is a consequence or payment they have to bear for their own errors or wrongdoings. It suggests a sense of personal accountability and responsibility for the negative outcome they are experiencing."""
             },
             {
                 "role": "user",
